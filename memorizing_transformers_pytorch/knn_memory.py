d1a9509ff5c2447afc986254d77d5c9705eb1935
Lennart Keller <lennartkeller@gmail.com>
2023-04-14 10:08:18 +0200

    revert memory dir change

--- memorizing_transformers_pytorch/knn_memory.py
+++ memorizing_transformers_pytorch/knn_memory.py
@@ -291,7 +291,7 @@ class KNNMemoryList(list):
         num_memory_layers,
         memories_directory = DEFAULT_KNN_MEMORY_MEMMAP_DIRECTORY
     ):
-        memories_path = Path(memories_directory) / str(random.random())
+        memories_path = Path(memories_directory)
         memories_path.mkdir(exist_ok = True, parents = True)
 
         def inner(*args, **kwargs):
